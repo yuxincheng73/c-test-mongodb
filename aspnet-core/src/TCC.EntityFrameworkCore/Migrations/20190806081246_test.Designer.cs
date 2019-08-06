6d8d0e53760a7106f88c956364d4287411931de1
Cheng Yu Xin <chengyuxin@trustmechain.com>
2019-08-06 16:25:12 +0800

    afdasdf

--- aspnet-core/src/TCC.EntityFrameworkCore/Migrations/20190806081246_test.Designer.cs
+++ aspnet-core/src/TCC.EntityFrameworkCore/Migrations/20190806081246_test.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TCC.EntityFrameworkCore;
 
 namespace TCC.Migrations
 {
     [DbContext(typeof(TCCDbContext))]
-    partial class TCCDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190806081246_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/TCC.EntityFrameworkCore/Migrations/20190806081246_test.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TCC.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Brands",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Brands",
+                newName: "id");
+        }
+    }
+}
